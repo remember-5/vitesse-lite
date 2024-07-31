347398cfe24fbcbdebcb2ff3b38a0e2488f8565a
wangjiahao <1332661444@qq.com>
2024-07-31 11:55:23 +0800

    feat: add dockerfile and ci file, add lodash/echarts/dayjs/datav

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import DataVVue3 from '@kjgl77/datav-vue3'
 import App from './App.vue'
-
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
@@ -10,5 +10,6 @@ const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 })
+app.use(DataVVue3)
 app.use(router)
 app.mount('#app')
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,66 @@
 <script setup lang="ts" generic="T extends any, O extends any">
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { PieChart } from 'echarts/charts'
+import {
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+} from 'echarts/components'
+import VChart, { THEME_KEY } from 'vue-echarts'
+import { provide, ref } from 'vue'
+
 defineOptions({
   name: 'IndexPage',
 })
 
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+])
+
+provide(THEME_KEY, 'dark')
+
+const option = ref({
+  title: {
+    text: 'Traffic Sources',
+    left: 'center',
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: '{a} <br/>{b} : {c} ({d}%)',
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left',
+    data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+  },
+  series: [
+    {
+      name: 'Traffic Sources',
+      type: 'pie',
+      radius: '55%',
+      center: ['50%', '60%'],
+      data: [
+        { value: 335, name: 'Direct' },
+        { value: 310, name: 'Email' },
+        { value: 234, name: 'Ad Networks' },
+        { value: 135, name: 'Video Ads' },
+        { value: 1548, name: 'Search Engines' },
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+  ],
+})
 const name = ref('')
 
 const router = useRouter()
@@ -20,6 +78,15 @@ function go() {
         Vitesse Lite
       </a>
     </p>
+    <el-button type="primary">
+      Default
+    </el-button>
+    <dv-loading>
+      <div color-white>
+        Loading...
+      </div>
+    </dv-loading>
+    <VChart class="chart" :option="option" />
     <p>
       <em text-sm op75>Opinionated Vite Starter Template</em>
     </p>
@@ -44,3 +111,9 @@ function go() {
     </div>
   </div>
 </template>
+
+<style scoped>
+.chart {
+  height: 400px;
+}
+</style>
